7f49ef5e518c179efc3d67cd83e33e76da8a384f
Ivan Tymchenko <tymchenkolviv@gmail.com>
2024-08-17 21:40:50 +0200

    created cache

---
+++ src/main/java/com/verygoodbank/tes/cache/LocalCache.java
@@ -0,0 +1,30 @@
+package com.verygoodbank.tes.cache;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public interface LocalCache {
+
+    static String getProductName(String productId) {
+        return Optional.ofNullable(cache().get(productId))
+                .orElseThrow(() ->
+                        new RuntimeException(String.format("No product with productId: %s found", productId)));
+    }
+
+    private static Map<Integer, String> cache() {
+        Map<Integer, String> productNameCache = new ConcurrentHashMap();
+
+        productNameCache.put(1, "Treasury Bills Domestic");
+        productNameCache.put(2, "Corporate Bonds Domestic");
+        productNameCache.put(3, "REPO Domestic");
+        productNameCache.put(4, "Interest rate swaps International");
+        productNameCache.put(5, "OTC Index Option");
+        productNameCache.put(6, "Currency Options");
+        productNameCache.put(7, "Reverse Repos International");
+        productNameCache.put(8, "REPO International");
+        productNameCache.put(9, "766A_CORP BD");
+        productNameCache.put(10, "766B_CORP BD");
+        return productNameCache;
+    }
+}
